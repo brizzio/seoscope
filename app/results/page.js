8f473eba0605b32cf261b31c4eb788c6ba442f21
brizzio <fabrizio.salvade@gmail.com>
2025-12-11 15:33:14 -0300

    results revalidate

--- app/results/page.js
+++ app/results/page.js
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 function parseNumber(value) {
   if (!value) return null;
